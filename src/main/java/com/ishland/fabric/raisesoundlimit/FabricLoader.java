011b7d260a206dc3fcab4bea33b2c69d240ce6d0
ishland <ishlandmc@yeah.net>
2020-07-30 23:30:50 +0800

    Initial modification

---
+++ src/main/java/com/ishland/fabric/raisesoundlimit/FabricLoader.java
@@ -0,0 +1,10 @@
+package com.ishland.fabric.raisesoundlimit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricLoader implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
