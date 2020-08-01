b1e0fcacb2b2aec99c884780c9e59d848e774a3d
ishland <ishlandmc@yeah.net>
2020-08-01 21:38:09 +0800

    Dynamically extend size

--- src/main/java/com/ishland/fabric/raisesoundlimit/mixin/MixinSoundEngineSourceSetImpl.java
+++ src/main/java/com/ishland/fabric/raisesoundlimit/mixin/MixinSoundEngineSourceSetImpl.java
@@ -1,9 +1,16 @@
 package com.ishland.fabric.raisesoundlimit.mixin;
 
 import com.ishland.fabric.raisesoundlimit.mixininterface.ISoundEngineSourceSetImpl;
+import com.ishland.fabric.raisesoundlimit.mixininterface.ISoundManager;
+import com.ishland.fabric.raisesoundlimit.mixininterface.ISoundSystem;
+import com.ishland.fabric.raisesoundlimit.sound.PooledSoundSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.Source;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net.minecraft.client.sound.SoundEngine$SourceSetImpl")
 public abstract class MixinSoundEngineSourceSetImpl implements ISoundEngineSourceSetImpl {
@@ -21,5 +28,18 @@ public abstract class MixinSoundEngineSourceSetImpl implements ISoundEngineSourc
     @Override
     public int impl$getSourceCount() {
         return getSourceCount();
+    }
+
+    @Inject(
+            method = "createSource",
+            at = @At(
+                    value = "RETURN"
+            )
+    )
+    public void onPostCreateSource(CallbackInfoReturnable<Source> cir) throws Exception {
+        if (cir.getReturnValue() == null) {
+            ((ISoundManager)MinecraftClient.getInstance().getSoundManager())
+                    .getSoundSystem().tryExtendSize();
+        }
     }
 }
